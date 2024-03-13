3ff0462ba95b69888b7e6cfc6b8ce7c1c3394c97
prasanna.v@kryptostech.com <Prasanna.V@KTP-RLT-KI2032.kryptos.tech>
2024-03-13 12:51:57 +0530

    Initial Commit

---
+++ src/main/java/Azure_DevOps_Project/Azure_DevOps/App.java
@@ -0,0 +1,13 @@
+package Azure_DevOps_Project.Azure_DevOps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
